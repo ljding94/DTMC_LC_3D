fixed initialized E calculation

analyze/Oplot.py
@@ -26,7 +26,7 @@ def Os_pars_plot(foldername, pars,par_nm,par_dg, mode):
             else:
                 f2rtail+="_"+par_nm[j]+"%.*f"%(par_dg[j],par_dealing[j])
                 label +=par_nm[j]+"%.*f,"%(par_dg[j],par_dealing[j])
-        f2rtail+="_ana.txt"
+        f2rtail+="_ana.csv"
         head = "/O_"
         filename = foldername + head+f2rtail
         data.append(np.loadtxt(filename, skiprows=1,delimiter=",", unpack=True))
@@ -96,7 +96,8 @@ def Os_pars_plot(foldername, pars,par_nm,par_dg, mode):
     O_cpar_plot(axs[7,0], uuc_ave, uuc_err, O_label, "uuc", r"$u_c=\left<(u_i\times u_j)\cdot\hat{r}_{ij} (u_i\cdot u_j)\right>_{(i,j)}$",cpar, colors, alphas)
     #plot the differential if mode is q
     O_cpar_plot(axs[8,0], un2_ave, un2_err, O_label, "un2", r"$u_n=\left<(u_i\cdot n_i)^2\right>_{i}$",cpar, colors, alphas)
-    O_cpar_plot(axs[9,0], un2p_ave, un2p_err, O_label, "un2p", r"$u_n=\left<(u_i\cdot n_i)^2\right>_{i}$",cpar, colors, alphas)
+    O_cpar_plot(axs[9,0], un2p_ave, un2p_err, O_label, "un2p", r"$u_n(p)=\left<(u_i\cdot n_i)^2\right>_{i}$",cpar, colors, alphas)
+    O_cpar_plot(axs[9,1], un2_ave-un2p_ave, np.zeros(len(un2_ave)), O_label, "un2-un2p", r"$u_n-u_n(p)$",cpar, colors, alphas)
     if(mode=="q"):
         O_cpar_plot(axs[7,1], uuc_grad, uuc_grad_err, O_label, "uuc_grad", r"$\partial u_c /\partial q$",cpar, colors, alphas)
         O_cpar_plot(axs[8,1], un2_grad, un2_grad_err, O_label, "un2_grad", r"$\partial u_n /\partial q$",cpar, colors, alphas)
@@ -130,7 +131,7 @@ def Geig_pars_plot(foldername, pars,par_nm,par_dg, mode):
             else:
                 f2rtail+="_"+par_nm[j]+"%.*f"%(par_dg[j],par_dealing[j])
                 label +=par_nm[j]+"%.*f,"%(par_dg[j],par_dealing[j])
-        f2rtail+="_ana.txt"
+        f2rtail+="_ana.csv"
         filename = foldername+f2rtail
         data.append(np.loadtxt(filename, skiprows=1,delimiter=",", unpack=True))
         O_label.append(label)

analyze/main.py
@@ -15,35 +15,35 @@ def main():
     #config_plot_xyz("../data/scratch_local/State_N200_imod1_Ne2_kar100_lam6.0_Kd5.0_q1.0_Cn5.0_kard0.0_init.txt",mesh=1)
     #return 0
 
-    foldername = "../data/Ne2/Apr26_2021" # kinda switch to Ne2 simulation for next paper
+    foldername = "../data/Ne2/Apr28_2021" # kinda switch to Ne2 simulation for next paper
     print("analyzing "+foldername)
-    N = 300
-    imod=3 # 1 for rhombus, 2 disk, 3 cylinder
-    Ne=2
+    N = 241
+    imod=2 # 1 for rhombus, 2 disk, 3 cylinder
+    Ne=1
     kar = 100
     karg=0.0
-    lam=4.0
+    lam=5.0
     lams=np.arange(4.0,9.1,1.0)
-    Kd=5
-    qs=np.arange(0.0,2.01,0.2)
-    Cn=5.0
-    Cnp=2.0
-    rCnps=np.arange(0.0,1.01,0.1)
+    Kd=9.0
+    Cn=Kd
+    q=0.1
+    Cnps=np.arange(2.0,10.1,0.5)
+    rCnps=np.arange(0.0,1.01,0.2)
     pars = []
     for rCnp in rCnps:
-        pars.append([N, imod,Ne, kar, lam, Kd, qs, Cn, Cnp, rCnp])
+        pars.append([N, imod,Ne, kar, lam, Kd, q, Cn, Cnps, rCnp])
     par_nm = ["N","imod", "Ne", "kar","lam","Kd","q", "Cn", "Cnp", "rCnp"]
     par_dg = [0,0,0,0,1,1,1,1,1,1] # nsumber of digit for each
-    mod="q"
+    mod="Cnp"
     for i in range(len(pars)):
         print("analyzing",pars[i])
         N, imod, Ne, kar, lam, Kd, q,Cn,Cnp,rCnp = pars[i]
-        #O_stat_ana(foldername,pars[i],par_nm,par_dg, mode=mod, tau_c=6)
+        O_stat_ana(foldername,pars[i],par_nm,par_dg, mode=mod, tau_c=6)
         if(1):
             pass
-            #Gij_stat_ana(foldername,pars[i],par_nm,par_dg,mode=mod, tau_c=6)
+            Gij_stat_ana(foldername,pars[i],par_nm,par_dg,mode=mod, tau_c=6)
 
-        for q in qs[::2]:
+        for Cnp in Cnps[::2]:
             if(i%1==0):
                 pass
                 filename = foldername + "/State_N%.0f_imod%.0f_Ne%.0f_kar%.0f_lam%.1f_Kd%.1f_q%.1f_Cn%.1f_Cnp%.1f_rCnp%.1f.csv" % (N, imod,Ne, kar,lam,Kd,q,Cn,Cnp,rCnp)