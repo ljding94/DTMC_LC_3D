fixed bending energy calculation  mistake in ho_metropolis

analyze/analyze.py
@@ -24,6 +24,7 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
     I2H_ave, I2H_tau, I2H_err = [], [], []
     I2H2_ave, I2H2_tau, I2H2_err = [], [], []
     phiHp_ave,phiHp_err,phiHp_tau = [], [], [] # phiH per bead
+    phiH2p_ave,phiH2p_err, phiH2p_tau = [],[],[] # phiH interaction per bond
     I2H2dis_ave, I2H2dis_tau, I2H2dis_err = [], [], []
     IK_ave, IK_tau, IK_err = [], [], []
     p2uu_ave, p2uu_tau, p2uu_err = [], [], []
@@ -56,8 +57,9 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
         E = data[0]/N
         Les = data[1:1+Ne]
         #Leuns = data[1+Ne:1+2*Ne]
-        IdA,I2H,I2H2,phiH,I2H2dis,IK,Tp2uu,Tuuc,Bond_num,Tun2 = data[1+Ne:]
+        IdA,I2H,I2H2,phiH,TphiH2,I2H2dis,IK,Tp2uu,Tuuc,Bond_num,Tun2 = data[1+Ne:]
         phiHp = phiH/N
+        phiH2p = TphiH2/Bond_num
         p2uu = Tp2uu/Bond_num
         uuc = Tuuc/Bond_num
         N =par[find_cpar_ind(par_nm,"N")]
@@ -144,6 +146,13 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
         phiHp_tau.append(tau)
         phiHp_err.append(np.sqrt(2 * tau / len(phiHp) * cov0))
 
+        # TphiH2
+        phiH2p_ave.append(np.average(phiH2p))
+        rho, cov0 = autocorrelation_function_fft(phiH2p)
+        tau, tau_err = tau_int_cal_rho(rho,tau_c)
+        phiH2p_tau.append(tau)
+        phiH2p_err.append(np.sqrt(2 * tau / len(phiH2p) * cov0))
+
         # I2H2dis
         I2H2dis_ave.append(np.average(I2H2dis))
         rho, cov0 = autocorrelation_function_fft(I2H2dis)
@@ -191,8 +200,6 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
     # only changed "lam" and "B" mode here, others waiting for further decision
     savefile = foldername + "/O_" + f2stail
 
-    # TODO: include writing I2H2dis analyzed data into the ana file
-    # TODO: include phiH analyzed data
     with open(savefile, "w") as f:
         f.write(mode+",Ep_ave,Ep_tau,Ep_err")
         # p stands for per bead
@@ -201,7 +208,7 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
         #for e in range(Ne):
         #    f.write(",Leuns_ave[%d],Leuns_tau[%d],Leuns_err[%d]"%(e,e,e))
 
-        f.write(",IdA_ave,IdA_tau,IdA_err,I2H_ave,I2H_tau,I2H_err,I2H2_ave,I2H2_tau,I2H2_err,phiHp_ave,phiHp_tau,phiHp_err,I2H2dis_ave,I2H2dis_tau,I2H2dis_err,IK_ave,IK_tau,IK_err,p2uu_ave,p2uu_tau,p2uu_err,uuc_ave,uuc_tau,uuc_err,un2_ave,un2_tau,un2_err")
+        f.write(",IdA_ave,IdA_tau,IdA_err,I2H_ave,I2H_tau,I2H_err,I2H2_ave,I2H2_tau,I2H2_err,phiHp_ave,phiHp_tau,phiHp_err,phiH2p_ave,phiH2p_tau,phiH2p_err,I2H2dis_ave,I2H2dis_tau,I2H2dis_err,IK_ave,IK_tau,IK_err,p2uu_ave,p2uu_tau,p2uu_err,uuc_ave,uuc_tau,uuc_err,un2_ave,un2_tau,un2_err")
         if(Ne==2):
             f.write(",Ledif_ave,Ledif_tau,Ledif_err")
         f.write("\n")
@@ -211,7 +218,7 @@ def O_stat_ana(foldername,par,par_nm,par_dg, mode, CnequalsKc=0, tau_c=6):
                 f.write(",%f,%f,%f"%(Les_ave[e][i],Les_tau[e][i], Les_err[e][i]))
             #for e in range(Ne):
             #    f.write(",%f,%f,%f"%(Leuns_ave[e][i],Leuns_tau[e][i], Leuns_err[e][i]))
-            f.write(",%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f"%(IdA_ave[i], IdA_tau[i], IdA_err[i],I2H_ave[i], I2H_tau[i], I2H_err[i],I2H2_ave[i], I2H2_tau[i], I2H2_err[i], phiHp_ave[i],phiHp_tau[i],phiHp_err[i],I2H2dis_ave[i],I2H2dis_tau[i],I2H2dis_err[i], IK_ave[i], IK_tau[i], IK_err[i], p2uu_ave[i], p2uu_tau[i], p2uu_err[i], uuc_ave[i], uuc_tau[i], uuc_err[i], un2_ave[i], un2_tau[i], un2_err[i]))
+            f.write(",%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f"%(IdA_ave[i], IdA_tau[i], IdA_err[i],I2H_ave[i], I2H_tau[i], I2H_err[i],I2H2_ave[i], I2H2_tau[i], I2H2_err[i], phiHp_ave[i],phiHp_tau[i],phiHp_err[i], phiH2p_ave[i],phiH2p_tau[i],phiH2p_err[i],I2H2dis_ave[i],I2H2dis_tau[i],I2H2dis_err[i], IK_ave[i], IK_tau[i], IK_err[i], p2uu_ave[i], p2uu_tau[i], p2uu_err[i], uuc_ave[i], uuc_tau[i], uuc_err[i], un2_ave[i], un2_tau[i], un2_err[i]))
             if(Ne==2):
                 f.write(",%f,%f,%f"%(Ledif_ave[i], Ledif_tau[i], Ledif_err[i]))
             f.write("\n")

analyze/main.py
@@ -12,37 +12,37 @@ from test_plot import *
 def main():
     print("hello! dtmc_lc analysis")
 
-    config_plot3D("../data/scratch_local/State_N200_imod1_Ne1_kar20_J2.0_C00.5_karg0.0_lam5.0_Kd0.0_q0.0_Cn0.0_init.csv",mesh=1,rod=0,fnormal=1)
-    config_plot3D("../data/scratch_local/State_N200_imod1_Ne1_kar20_J2.0_C00.5_karg0.0_lam5.0_Kd0.0_q0.0_Cn0.0.csv",mesh=1,rod=0,fnormal=0)
+    #config_plot3D("../data/scratch_local/State_N200_imod1_Ne1_kar20_J2.0_C00.5_karg0.0_lam5.0_Kd0.0_q0.0_Cn0.0_init.csv",mesh=1,rod=0,fnormal=1)
+    #config_plot3D("../data/scratch_local/State_N200_imod1_Ne1_kar20_J2.0_C00.5_karg0.0_lam5.0_Kd0.0_q0.0_Cn0.0.csv",mesh=1,rod=0,fnormal=0)
     #config_plot3D("../data/Ne2/Aug4_2021_1/State_N300_imod3_Ne2_kar50_C00.0_karg0.0_lam8.0_Kd10.0_q2.0_Cn20.0.csv",mesh=1,rod=0)
 
-    return 0
+    #return 0
 
     #foldername = "../data/Ne2/Apr29_2021" # kinda switch to Ne2 simulation for next paper
     #foldername = "../data/Ne1/May4_2021" # mobius strip~,
-    foldername = "../data/Ne2/Aug14_2021"
+    foldername = "../data/Ne2/Aug17_2021"
     print("analyzing "+foldername)
     N = 300
-    imod=3 # 1 for rhombus, 2 disk, 3 cylinder, 4 for mobius strip
-    Ne=2
+    imod=1 # 1 for rhombus, 2 disk, 3 cylinder, 4 for mobius strip
+    Ne=1
     kar = 100
+    Js = np.arange(0.03,0.61,0.03)
     C0s = np.arange(0.0,0.91,0.1)
     karg = 0.0
     lam = 5.0
-    Kd = 8.0
-    qs = np.arange(0.3,3.1,0.3)
-    #qs=np.arange(0.2,1.1,0.2)
-    Cn = 8.0
+    Kd = 0.0
+    q = 0.0
+    Cn = 0.0
     pars = []
     pars1,pars2 = [],[]
-    for q in qs:
+    for C0 in C0s:
         Cn = Kd
-        pars.append([N, imod, Ne, kar, C0s, karg, lam, Kd, q, Cn])
+        pars.append([N, imod, Ne, kar, Js, C0, karg, lam, Kd, q, Cn])
         #pars1.append([N, 1, 1, kar, C0, karg, lam, Kd, qs, Cn])
         #pars2.append([N, 3, 2, kar, C0, karg, lam, Kd, qs, Cn])
-    par_nm = ["N","imod", "Ne", "kar", "C0","karg", "lam","Kd","q", "Cn"]
-    par_dg = [0,0,0,0,1,1,1,1,1,1] # number of digit for each
-    mod="C0"
+    par_nm = ["N","imod", "Ne", "kar", "J" ,"C0","karg", "lam","Kd","q", "Cn"]
+    par_dg = [0,0,0,0,2,1,1,1,1,1,1] # number of digit for each
+    mod="J"
 
     # run test plot
     #E_compare(foldername, pars1, pars2, par_nm, par_dg, mod)
@@ -50,19 +50,19 @@ def main():
 
     for i in range(len(pars)):
         print("analyzing",pars[i])
-        N, imod, Ne, kar, C0, karg, lam, Kd, q, Cn = pars[i]
+        N, imod, Ne, kar, J, C0, karg, lam, Kd, q, Cn = pars[i]
         O_stat_ana(foldername,pars[i],par_nm,par_dg, mode=mod, CnequalsKc=0, tau_c=6)
         if(1):
             pass
             Gij_stat_ana(foldername,pars[i],par_nm,par_dg,mode=mod, tau_c=6)
 
-        for C0 in C0s[::1]:
+        for J in Js[::1]:
             if(i%1==0):
                 pass
-                filename = foldername + "/State_N%.0f_imod%.0f_Ne%.0f_kar%.0f_C0%.1f_karg%.1f_lam%.1f_Kd%.1f_q%.1f_Cn%.1f.csv" % (N, imod,Ne,kar,C0,karg,lam,Kd,q,Cn)
-                ctag = r"$C_0=%.1f,K_d=%.1f,q=%.1f$" %(C0,Kd,q)
+                filename = foldername + "/State_N%.0f_imod%.0f_Ne%.0f_kar%.0f_J%.2f_C0%.1f_karg%.1f_lam%.1f_Kd%.1f_q%.1f_Cn%.1f.csv" % (N, imod,Ne,kar,J,C0,karg,lam,Kd,q,Cn)
+                ctag = r"$J=%.2f,C_0=%.1f$" %(J,C0)
                 #config_plot_xyz(filename,mesh=0,rod=1,tag=ctag,Format="png")
-                config_plot_xyz(filename,mesh=1,rod=0,tag=ctag,Format="png")
+                #config_plot_xyz(filename,mesh=1,rod=0,tag=ctag,Format="png")
 
     colors = None
     alphas = None

analyze/plot.py
@@ -37,8 +37,8 @@ def config_plot_xyz(filename,mesh=0,rod=1,cvt_map="",cmap_smooth=0,tag="", Forma
     data = np.loadtxt(filename, skiprows=6, delimiter=",", unpack=True)
 
     #x,y,z,sx,sy,sz,nx,ny,nz,dA,d2H,ds,dAK,un2,enum, en0, en1 = data[:18]
-    x,y,z,sx,sy,sz,phiH,dA,d2H,ds,dAK,un2,enum, en0, en1 = data[:15]
-    ns = np.transpose(data[15:])
+    x,y,z,sx,sy,sz,nx,ny,nz,phiH,dA,d2H,ds,dAK,un2,enum, en0, en1 = data[:18]
+    ns = np.transpose(data[18:])
     #sx,sy,sz=d*sx,d*sy,d*sz
     #x,y,z,sx,sy,sz, enum, en0, en1 = data[5:14]
     x_min, x_max = np.min(x),np.max(x)